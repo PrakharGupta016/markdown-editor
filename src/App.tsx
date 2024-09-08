08321667d060af7cc387fd2fa5309f7164c43238
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-09-08 13:55:46 +0530

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Textarea } from "./components/common/shadcn/TextArea";
+
 
 
 function App() {
   return (
     <div className="flex ">
-
+        
+        <Textarea/>
     </div> 
   );
 }
