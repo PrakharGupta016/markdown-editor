7a9cc27630e93fe4e286dbd625dfbe1bbaf9b94c
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-09-08 20:53:40 +0530

    handler parsing

--- src/components/markdown-editor/markdownEditor.tsx
+++ src/components/markdown-editor/markdownEditor.tsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 import { Textarea } from "../common/shadcn/TextArea";
 import Layers from "../common/styled-components/layers";
+import parse from "html-react-parser";
 
 const trimHashes = (hashString: string) => {
   const ch: string = "#";
   const sz: number = hashString.length;
   let idx = -1;
+  let text = "";
   for (let i = 0; i < sz; i++) {
     if (hashString.charAt(i) == ch) {
       idx = i;
     } else break;
   }
-  if (idx == -1) return hashString;
-  if (idx <= 2) {
-    return hashString.substring(idx + 1, sz);
+
+  if (idx < 6 && idx >= 0) {
+    text =
+      `<h${idx + 1}>` + hashString.substring(idx + 1, sz) + `</h${idx + 1}>`;
   }
-  return hashString;
+
+  text = text.replace(/\*\*\*(.*?)\*\*\*/g, "<strong><em>$1</em></strong>");
+
+  // Convert Bold
+  text = text.replace(/\*\*(.*?)\*\*|__(.*?)__/g, "<strong>$1$2</strong>");
+
+  // Convert Italic
+  text = text.replace(/\*(.*?)\*|_(.*?)_/g, "<em>$1$2</em>");
+
+  return text;
 };
 
 const MarkDownEditor = () => {
@@ -23,17 +35,13 @@ const MarkDownEditor = () => {
   const [result, setResult] = useState("");
 
   const convertMarkdown = (markdown: string) => {
-   
     const lines = markdownString.split("\n");
-    const filteredLines = lines.filter((val) => val != "");
-    let result = filteredLines;
-    for (let i = 0 ; i <filteredLines.length ; i++) {
-            result[i] = trimHashes(filteredLines[i]);
-    }
-    const stringResult = result.join("\n")
-    console.log(stringResult)
-    setResult(stringResult);
-    setMarkDownString(markdown);
+    const filteredLines = lines.filter((val) => val !== "");
+    let result = filteredLines.map((val) => trimHashes(val));
+    const stringResult = result.join("\n");
+
+    setResult((prev) => stringResult);
+    setMarkDownString((prev) => markdown);
   };
 
   return (
@@ -45,7 +53,7 @@ const MarkDownEditor = () => {
           onChange={(e) => convertMarkdown(e.target.value)}
         />
         <h1>result</h1>
-        <Textarea value={result}/>
+        <div>{parse(result)}</div>
       </Layers>
     </>
   );
