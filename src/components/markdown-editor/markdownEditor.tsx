c0dfff88566a3da45afd7fc07410ffb989dbd7e7
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-09-14 00:55:27 +0530

    grid and linebreak added

--- src/components/markdown-editor/markdownEditor.tsx
+++ src/components/markdown-editor/markdownEditor.tsx
@@ -2,18 +2,12 @@ import { useState } from "react";
 import { Textarea } from "../common/shadcn/TextArea";
 import Layers from "../common/styled-components/layers";
 import parse from "html-react-parser";
-
-
-const headingStyles: { [key: number]: string } = {
-  1: "text-3xl font-bold",
-  2: "text-2xl font-semibold",
-  3: "text-xl font-semibold",
-  4: "text-lg font-medium",
-  5: "text-base font-medium",
-  6: "text-sm font-medium",
-};
+import { headingStyles } from "@/helpers/helpers";
 
 const trimHashes = (hashString: string) => {
+  if (hashString == "") {
+    return "<br/>";
+  }
   const ch: string = "#";
   const sz: number = hashString.length;
   let idx = -1;
@@ -26,7 +20,9 @@ const trimHashes = (hashString: string) => {
 
   if (idx < 6 && idx >= 0) {
     text =
-      `<h${idx + 1} className=${headingStyles[idx+1]}>` + hashString.substring(idx + 1, sz) + `</h${idx + 1}>`;
+      `<h${idx + 1} className=${headingStyles[idx + 1]}>` +
+      hashString.substring(idx + 1, sz) +
+      `</h${idx + 1}>`;
   }
 
   text = text.replace(/\*\*\*(.*?)\*\*\*/g, "<strong><em>$1</em></strong>");
@@ -36,6 +32,9 @@ const trimHashes = (hashString: string) => {
 
   // Convert Italic
   text = text.replace(/\*(.*?)\*|_(.*?)_/g, "<em>$1$2</em>");
+  if (idx == -1) {
+    text = "<p>" + text + "</p>";
+  }
 
   return text;
 };
@@ -47,7 +46,7 @@ const MarkDownEditor = () => {
   const convertMarkdown = (markdown: string) => {
     const lines = markdown.split("\n");
     let result = lines.map((val) => trimHashes(val));
-    console.log(result)
+    console.log(result);
     setResult((prev) => result);
     setMarkDownString((prev) => markdown);
   };
@@ -55,14 +54,23 @@ const MarkDownEditor = () => {
   return (
     <>
       <Layers>
-        <div className="p-4 grid ">
-        <h1>Enter your markdown here </h1>
-        <Textarea
-          value={markdownString}
-          onChange={(e) => convertMarkdown(e.target.value)}
-        />
-        <h1>result</h1>
-        <div>{result.map((val)=>(<div>{parse(val)}</div>))}</div>
+        <div className="p-4 grid grid-cols-2 gap-4">
+          <div>
+            <h1 className={headingStyles[2]}>Enter your markdown here </h1>
+            <Textarea
+              value={markdownString}
+              onChange={(e) => convertMarkdown(e.target.value)}
+              rows={10}
+            />
+          </div>
+          <div>
+            <h1 className={headingStyles[2]}>Preview</h1>
+            <div>
+              {result.map((val) => (
+                <>{parse(val)}</>
+              ))}
+            </div>
+          </div>
         </div>
       </Layers>
     </>
